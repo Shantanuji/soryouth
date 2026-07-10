ec08a9790e601c135d6c265a5609e27661cd1924
shantanukumar2026 <shantanukumar.20026@gmail.com>
2026-07-10 13:29:20 +0530

    update deploy script

--- deploy-to-server.bat
+++ deploy-to-server.bat
@@ -7,7 +7,7 @@ echo Connecting to server and updating everything...
 echo This will take a couple of minutes...
 echo.
 
-ssh -i hostinger_rsa root@187.127.188.247 "cd /var/www/soryouth-crm && echo 1. Pulling Latest Code... && git pull origin main && echo 2. Installing Node Modules... && npm install && echo 3. Updating Database... && npx prisma generate && npx prisma db push && echo 4. Building Next.js Website... && npm run build && echo 5. Restarting App... && pm2 restart soryouth-crm"
+ssh -i "%~dp0hostinger_rsa" root@187.127.188.247 "cd /var/www/soryouth-crm && echo 1. Pulling Latest Code... && git pull origin main && echo 2. Installing Node Modules... && npm install && echo 3. Updating Database... && npx prisma generate && npx prisma db push && echo 4. Building Next.js Website... && npm run build && echo 5. Restarting App... && pm2 restart soryouth-crm"
 
 echo.
 echo ===================================================
