2a33d05c2b127eca473727bd8c11c8eb1b7e370d
shantanukumar2026 <shantanukumar.20026@gmail.com>
2026-08-21 10:11:00 +0530

    perf(build): optimize next.config.ts for fast production builds

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   experimental: {
     serverActions: {
       bodySizeLimit: '100mb',
     },
-    middlewareClientMaxBodySize: '100mb',
   },
   images: {
     remotePatterns: [
